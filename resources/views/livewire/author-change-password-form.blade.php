5c209fb01d0ce03be2d0449f94ef9b425146b1f3
mictest23 <mictestone@gmail.com>
2023-04-28 10:48:35 +0800

    password change functionalities added

---
+++ resources/views/livewire/author-change-password-form.blade.php
@@ -0,0 +1,32 @@
+<div>
+    
+
+    <form method="POST" wire:submit.prevent="changePassword()">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="mb-3">
+              <label class="form-label">Current Password</label>
+              <input type="password" class="form-control" placeholder="Current Password" wire:model="current_password">
+              <span class="text-danger">@error('current_password'){{ $message }}@enderror</span>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="mb-3">
+              <label class="form-label">New Password</label>
+              <input type="password" class="form-control" placeholder="New Password" wire:model="new_password">
+              <span class="text-danger">@error('new_password'){{ $message }}@enderror</span>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="mb-3">
+              <label class="form-label">Confirm Password</label>
+              <input type="password" class="form-control" placeholder="Re-type Password" wire:model="confirm_new_password">
+              <span class="text-danger">@error('confirm_new_password'){{ $message }}@enderror</span>
+            </div>
+          </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Change password</button>
+      </form>
+
+
+</div>
